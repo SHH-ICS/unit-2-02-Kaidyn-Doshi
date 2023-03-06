c4284567216e316ef6f25546a4950a88e24d9fd6
Kaidyn <124370935+Kaidyn-Doshi@users.noreply.github.com>
2023-03-06 15:53:45 +0000

    code

--- program.py
+++ program.py
@@ -2,4 +2,7 @@
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
 def myFunction():
-  print("Hello world!")
+  myText = ("hello world!")
+  return(myText)
+
+print(myFunction())
