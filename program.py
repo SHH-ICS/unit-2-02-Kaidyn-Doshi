dd73dca011a347a7c6a8d6b738ab82498cfb8676
Kaidyn <124370935+Kaidyn-Doshi@users.noreply.github.com>
2023-03-08 15:01:29 +0000

    commit

--- program.py
+++ program.py
@@ -1,9 +1,15 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
-
-def say_hi (name):
-    name = input("What is your name? ")
-    if name == input(name):
-        return("Hello" + name + "!")
-    else:
-        return("No name? ")
+def politeGreeting(name):
+  if name == "Nina":
+    return("nerd emoji")
+  if name == "Viridian":
+    return("monkey")
+  else:
+    return("Hello, " + name + "!")
+  if name == "bozo":
+    return("bozo")
+  else:
+    return("Hello, " + name + "!")
+name = input("Please enter your name:")        
+print(politeGreeting(name))
